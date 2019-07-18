cbc3c41504a9e2b445bee886f02d954560be041f
Vinícius Nascimento <viniciusnascimento__@outlook.com>
2019-07-17 22:05:46 -0300

    Fix view path.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const port = process.env.PORT || 3000
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
